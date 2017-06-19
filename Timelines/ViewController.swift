5a3a1b97adef7416b9aee3984ffec02e997bbf8d
Mark Huot <mark@markhuot.com>
2017-06-19 09:35:12 -0400

    Initial Commit

---
+++ Timelines/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timelines
+//
+//  Created by Mark Huot on 6/19/17.
+//  Copyright © 2017 Mark Huot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
